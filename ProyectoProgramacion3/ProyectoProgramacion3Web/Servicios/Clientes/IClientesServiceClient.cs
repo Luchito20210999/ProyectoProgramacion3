73913c7f9fe3887832dcfeab8c405ad4db657e80
Jeremy-Campos9 <jor040campos@gmail.com>
2026-06-27 22:12:16 -0500

    QuitoFront

--- ProyectoProgramacion3/ProyectoProgramacion3Web/Servicios/Clientes/IClientesServiceClient.cs
+++
@@ -1,8 +0,0 @@
-using ProyectoProgramacion3Web.Servicios.Base;
-using ProyectoProgramacion3Web.ViewModels;
-
-namespace ProyectoProgramacion3Web.Servicios.Clientes;
-
-public interface IClientesServiceClient : IServiceClient<ClienteItem>
-{
-}
--- ProyectoProgramacion3/ProyectoProgramacion3Web/Servicios/ServiciosTuristicos/IServiciosServiceClient.cs
+++
@@ -1,8 +0,0 @@
-using ProyectoProgramacion3Web.Servicios.Base;
-using ProyectoProgramacion3Web.ViewModels;
-
-namespace ProyectoProgramacion3Web.Servicios.ServiciosTuristicos;
-
-public interface IServiciosServiceClient : IServiceClient<ServicioItem>
-{
-}
