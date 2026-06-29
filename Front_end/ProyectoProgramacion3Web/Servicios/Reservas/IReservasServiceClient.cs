42fe9cbe0c00076a63b5b0c65bc3972d795f1c0a
Luchito20210999 <a20210999@pucp.edu.pe>
2026-06-29 13:13:04 -0500

    Delete Front_end directory

--- Front_end/ProyectoProgramacion3Web/Servicios/Reservas/IReservasServiceClient.cs
+++
@@ -1,8 +0,0 @@
-using ProyectoProgramacion3Web.ViewModels;
-
-namespace ProyectoProgramacion3Web.Servicios.Reservas;
-
-public interface IReservasServiceClient
-{
-    List<ReservaItem> Listar();
-}
